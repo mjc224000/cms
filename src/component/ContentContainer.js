57a94e4680b098439e75aaca5986bc956d4be84d
Administrator <397938635@qq.com>
2018-05-14 09:44:50 +0800

    layout

---
+++ src/component/ContentContainer.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react'
+import { Layout } from 'antd';
+import '../style/ContentConstomer.css'
+const {Content} =Layout;
+export default  class _Content extends Component{
+    render(){
+        return( <Content/>)
+    }
+}
