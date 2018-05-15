957c1350160aeb8c5ba6528f5b221b1995d32552
Administrator <397938635@qq.com>
2018-05-15 10:55:12 +0800

    layout

--- src/index.js
+++ src/index.js
@@ -4,14 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route, Link, } from "react-router-dom";
-const aa=()=>{
-  return  (<div> aa</div>)
-}
-const Parent = () => (
-    <div>
-        <Route path='other' component={aa} />
-    </div>
-)
+
+
 const Index=()=>{
   return  (<Router >
           <Route path={'/'} component={App}>
