b55a99ffae06e93c4b68a4f98a66c4ab4bee1e9e
Administrator <397938635@qq.com>
2018-05-17 08:36:59 +0800

    layout

--- src/component/ContentContainer.js
+++ src/component/ContentContainer.js
@@ -4,14 +4,14 @@ import { BrowserRouter as Router, Route, Link, } from "react-router-dom";
 import '../style/ContentContainer.css'
 import FormNormalLogin from './form/FormNormal';
 import FormRejistration from './form/FormRegistration';
-import Ohter from './form/other'
+import Other from './form/other'
 const {Content} =Layout;
 export default  class _Content extends Component{
     render(){
         return( <Content>
             <Route path="/FormNormal" component={FormNormalLogin}/>
             <Route path={"/FormRegistration"} component={FormRejistration}/>
-            <Route path={"/Other"} component={Ohter}/>
+            <Route path={"/Other"} component={Other}/>
         </Content>)
     }
 }
